c2232424af9c6e1c5cb4fdcc49d15bd5435f8b04
thanhliem <phamvanliem26122002@gmail.com>
2023-09-17 09:13:57 +0700

    'init_project_be'

---
+++ BE/src/app.js
@@ -0,0 +1,31 @@
+// const express = require('express');
+import express from 'express'
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+require('dotenv').config();
+
+const app = express();
+
+//init middleware
+app.use(morgan("dev"))
+app.use(helmet()) //hidden framework using
+app.use(compression())
+
+//init db
+require('./dbs/init.mysql')
+// const { checkOverload } = require('./helpers/check.connect');
+// checkOverload();
+
+//init route
+app.get('/', (req, res, next) => {
+    const helloWorld = 'Hello word';
+
+    return res.status(200).json({
+        message: 'Hello word',
+        metaData: helloWorld.repeat(100000)
+    })
+})
+//handle errors
+
+module.exports = app;
